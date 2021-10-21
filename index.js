c769e048e236ab49bf8935b8e060d2a8bd18f305
Ziv Serphos <zivfromisrael@gmail.com>
2021-10-21 16:24:13 +0300

    compleated response format

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const router = require('./src/routers/pokemonRouter')
 const app = express();
 const port = 8080;
 
-app.use(express.json)
+//app.use(express.json())
 app.use("/pokemon" , router)
 
 
--- src/routers/pokemonRouter.js
+++ src/routers/pokemonRouter.js
@@ -3,13 +3,38 @@ const router = express.Router();
 const Pokedex = require("pokedex-promise-v2");
 const P = new Pokedex();
 
-router.get("/pokemon/query", (req, res) => {
-  P.getPokemonByName(req.query) // with Promise
-    .then(function (response) {
-      res.json(response);
+const types = (types) => {
+    let typeNames = [];
+    types.forEach((type) => {
+        typeNames.push(type.type.name)
+    })
+    return typeNames;
+} 
+
+const abilities = (abilities) => {
+    let abilitiesNames = [];
+    abilities.forEach((ability) => {
+        abilitiesNames.push(ability.ability.name)
+    })
+    return abilitiesNames
+}
+
+router.get("/:query", (req, res) => {
+    P.getPokemonByName(req.params.query) // with Promise
+    .then(function (pokemon) {
+      const pokemonDetails = {
+          name: pokemon.name,
+          height: pokemon.height,
+          weight: pokemon.weight,
+          types: types(pokemon.types),
+          front_pic: pokemon.sprites["front_default"],
+          back_pic: pokemon.sprites["back_default"],
+          abilities: abilities(pokemon.abilities)
+      }
+      res.json(pokemonDetails)
     });
 });
-router.get("/pokemon/get/:id", (req, res) => {
+router.get("/get/:id", (req, res) => {
   return res.json();
 });
 
