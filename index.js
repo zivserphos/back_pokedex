febe25e492a29b43c070722df4e262f7e406dc41
zivserphos <zivfromisrael@gmail.com>
2021-11-01 20:46:58 +0200

    env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const handleUsersReq = require('./handlers/handleUsersReq');
 const userRouter = require("./src/routers/userRouter")
 const errorHandler = require('./handlers/errorHandler');
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(cors({origin: "*"}))
 app.use(express.json())
